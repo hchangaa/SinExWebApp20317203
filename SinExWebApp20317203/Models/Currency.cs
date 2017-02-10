b908c2e889bc6c7a6216b570ecb4500b841e7a16
hchangaa <hchangaa@CSL4WK33>
2017-02-10 18:44:29 +0800

    initial

---
+++ SinExWebApp20317203/Models/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SinExWebApp20317203.Models
+{
+    [Table("Currency")]
+    public class Currency
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public virtual string CurrencyCode { get; set; }
+        public virtual int ExchangeRate { get; set; }
+        public virtual int hahahaExchangeRate { get; set; }
+
+    }
+}
---
+++ SinExWebApp20317203/Models/ServicePackageFee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace SinExWebApp20317203.Models
+{
+    [Table ("ServicePackageFee")]
+    public class ServicePackageFee
+    {
+        public virtual int ServicePackageFeeID { get; set; }
+        public virtual decimal Fee { get; set; }
+        public virtual decimal MinimumFee { get; set; }
+        public virtual int PackageTypeID { get; set; }
+        public virtual int ServiceTypeID { get; set; }
+        public virtual PackageType PackageType { get; set; }
+        public virtual ServiceType ServiceType { get; set; }
+    }
+}
